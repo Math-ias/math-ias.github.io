6fafca7e72c9ab111316b26729fdaa2a1167f733
Mathias Kools <mathiaskools@gmail.com>
2022-05-09 16:03:06 -0400

    Update day 7 graphs.

--- aoc2021/7.py
+++ aoc2021/7.py
@@ -1,15 +1,30 @@
 import leather 
 
+leather.theme.axis_title_color = "#000000"
+leather.theme.axis_title_font_size = 15
+leather.theme.axis_title_gap = 30
+leather.theme.background_color = "#ffffff"
+leather.theme.default_chart_height = 350
+leather.theme.default_chart_width = 600
 leather.theme.axis_title_font_family = "monospace"
-leather.theme.background_color="#ffffff"
+leather.theme.default_dot_radius = 7
+leather.theme.default_line_width = 7
+leather.theme.label_color = "#000000"
+leather.theme.tick_color = "#000000"
+leather.theme.tick_font_size = 15
+leather.theme.tick_size = 3
 leather.theme.tick_font_family = "monospace"
+leather.theme.tick_width = 1
+leather.theme.title_color = "#000000"
+leather.theme.title_font_size = 15
+leather.theme.zero_color = "#000000"
 leather.theme.title_font_family = "monospace"
 
 with open(".7.txt", "r") as fp:
     positions = list(map(lambda position: int(position), ",".join(fp.readlines()).split(",")))
 
-chart_one = leather.Chart("Total Cost VS Chosen Center (Part One)")
-chart_two = leather.Chart("Total Cost VS Chosen Center (Part Two)")
+chart_one = leather.Chart("Total Cost VS Chosen Center (Pt 1)")
+chart_two = leather.Chart("Total Cost VS Chosen Center (Pt 2)")
 
 part_one_points = []
 part_two_points = []
@@ -17,19 +32,18 @@ part_two_points = []
 for center in range(min(positions), max(positions) + 1):
     part_one_points.append(
         (center,
-        sum(map(lambda position: abs(position - center), positions)))
+        sum(map(lambda position: abs(position - center), positions))/1000)
     )
     part_two_points.append(
         (center,
-        sum(map(lambda position: ((position - center)**2 + abs(position - center)) >> 1, positions)))
+        sum(map(lambda position: ((position - center)**2 + abs(position - center)) >> 1, positions))/1000000)
     )
 
 chart_one.add_line(part_one_points)
 chart_one.add_x_axis(name="Center")
-chart_one.add_y_axis(name="Total Cost")
+chart_one.add_y_axis(name="Total Cost (thousands)")
 chart_two.add_line(part_two_points)
 chart_two.add_x_axis(name="Center")
-chart_two.add_y_axis(name="Total Cost")
+chart_two.add_y_axis(name="Total Cost (millions)")
 print(chart_one.to_svg())
 print(chart_two.to_svg())
-
